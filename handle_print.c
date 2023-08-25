700b1311f4be5c55c0a2dc29f4fcfacd0f04a792
NaMgwezane <oshunlutho@gmail.com>
2023-08-25 08:49:18 -0700

    printf.

--- handle_print.c
+++ handle_print.c
@@ -16,7 +16,7 @@
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[], int flags, int width, int precision, int size)
 {
 	int i, unknow_len = 0, printed_chars = -1; fmt_t fmt_types[] = {
-		{'c', print_char}, {'s'. print_string}, {'%', print_percent},
+		{'c', print_char}, {'s', print_string}, {'%', print_percent},
 		{'i', print_int}, {'d', print_int}, {'b', print_binary},
 		{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
 		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
@@ -31,12 +31,12 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[], int fla
 		if (fmt[*ind] == '\0')
 			return (-1);
 		unknow_len += write(1, "%%", 1);
-		if (fmt[*ind - 1] == '')
+		if (fmt[*ind - 1] == '\0')
 			unknow_len += write(1, " ", 1);
 		else if (width)
 		{
 			--(*ind);
-			while (fmt[*ind] != '' && fmt[*ind] != '%')
+			while (fmt[*ind] != '\0' && fmt[*ind] != '%')
 				--(*ind);
 			if (fmt[*ind]);
 			return (1);

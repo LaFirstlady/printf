8827757a075172bf026a129e753c62a40fc6d230
NaMgwezane <oshunlutho@gmail.com>
2023-08-25 08:13:55 -0700

    write_handlers

--- write_handlers.c
+++ write_handlers.c
@@ -228,13 +228,14 @@ int write_pointer(char buffer[], int ind, int length, int width, int flags, char
 				buffer[--padd_start] = extra_c;
 			buffer[1] = '0';
 			buffer[2] = 'x';
-			return (write(1, &buffer[padd_start], (padd_start) + write(1, &buffer[ind], length - (1 - (padd_start) - 2));
+			return (write(1, &buffer[padd_start], i - padd_start) + write(1, &buffer[ind], length - (1 - padd_start) - 2));
 		}
+		buffer[--ind] = 'x';
+		buffer[--ind] = '0';
+		if (extra_c)
+			buffer[--ind] = extra_c;
+		return (write(1, &buffer[ind], BUFF_SIZE - ind - 1));
 	}
-	buffer[--ind] = 'x';
-	buffer[--ind] = '0';
-	if (extra_c)
-		buffer[--ind] = extra_c;
-	return (write(1, &buffer[ind], BUFF_SIZE - ind - 1));
-	}
+}
+
 	
